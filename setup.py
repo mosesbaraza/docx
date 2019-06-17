1dc7060b0434b880cf3dfed0b81f5b63d0ec078d
Moses Baraza <clonesoftwares@gmail.com>
2019-06-17 12:23:23 +0300

    add module description

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='docx',
-    version='1.0.0',
+    version='2.0.0',
     author='Moses Baraza',
     author_email='clonesoftwares@gmail.com',
     packages=find_packages(),
     classifiers=[
-        'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3',
         ],
     )

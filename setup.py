0bfb8004a6a0a075d5eca3c3205f113e78c5a82a
Moses Baraza <clonesoftwares@gmail.com>
2019-06-18 13:33:29 +0300

    modify classifier attribute

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='docx',
     version='2.0.0',
-    description='python module to enable you read microsoft word .docx files',
     author='Moses Baraza',
     author_email='clonesoftwares@gmail.com',
     packages=find_packages(),
